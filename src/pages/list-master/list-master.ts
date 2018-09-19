1ddfa9e4ba4bd80f0ac6bed03775150a144b1882
Bruno Sessa <ing.brunosessa@gmail.com>
2018-09-19 14:55:53 -0300

    Ya funciona el searchbar

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, ModalController, NavController } from 'ionic-angular';
+import { IonicPage, ModalController, NavController, LoadingController } from 'ionic-angular';
 import { WsProvider } from '../../providers/ws/ws';
 import { User } from '../../models/user';
 
@@ -11,21 +11,43 @@ import { User } from '../../models/user';
 export class ListMasterPage {
 	users: any=[];
 
-	constructor(public navCtrl: NavController, public modalCtrl: ModalController, public ws: WsProvider) {
+	constructor(public navCtrl: NavController, public modalCtrl: ModalController, public loadingCtrl: LoadingController, public ws: WsProvider) {
+		this.getUsers();
 	}
 
 	getUsers() {
+		let loading = this.loadingCtrl.create({
+			content:'Cargando amigos'
+		});
+
+		loading.present();
+
 		this.ws.getUsers()
 		.then(data => {
 			this.users = data;
+			loading.dismiss();
 		});
 	}
 
-	/**
-	* The view loaded, let's query our items for the list
-	*/
+	searchUsers(ev: any) {
+		
+		// set val to the value of the searchbar
+		const val = ev.target.value;
+
+		// if the value is an empty string don't filter the items
+		if (val && val.trim() != '') {
+			this.users = this.users.filter((user) => {
+				return (user.nombre.toLowerCase().indexOf(val.toLowerCase()) > -1);
+			})
+		}
+		else {
+			this.getUsers();
+		}
+
+	}
+
 	ionViewDidLoad() {
-		this.getUsers();
+
 	}
 
 	//Agregar contacto
